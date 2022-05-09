5859c335b53f75347ae02387c8f05eadbc4274ac
lucas_baiao <lucas.baiao@edirectinsure.com>
2022-05-09 23:52:36 +0100

    add new test for poetry

--- src/tests/test_tddpythondemo.py
+++ src/tests/test_tddpythondemo.py
@@ -11,3 +11,9 @@ def test_request_exist():
     localreq = requestTest("http://www.google.com")
     req_result = localreq.make_request()
     assert req_result == 200
+
+@pytest.mark.xfail
+def test_request_fails():
+    localreq = requestTest("http://www.google.com")
+    req_result = localreq.make_request()
+    assert req_result == 400
